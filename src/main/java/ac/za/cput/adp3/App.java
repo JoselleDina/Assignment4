c67e87bfcc3030252acc27d92b678cc480b0dcd1
Joselle Dinana <joselledina@gmail.com>
2020-03-17 17:28:02 +0200

    committed

--- src/main/java/ac/za/cput/adp3/App.java
+++ src/main/java/ac/za/cput/adp3/App.java
@@ -2,6 +2,10 @@ package ac.za.cput.adp3;
 import ac.za.cput.adp3.domain.Employee;
 
 import java.util.*;
+import java.util.LinkedHashMap;
+
+import static java.util.stream.Collectors.toMap;
+
 
 /**
  * Hello world!
@@ -17,20 +21,14 @@ public class App
         empList.add(new Employee(02, "josy", "dina"));
         empList.add(new Employee(03, "nelcia", "niangui"));
 
-
-
         for(int i = 0; empList.size() > i; i++)
         {
             System.out.println(empList.get(i));
         }
 
-
-
-
-
-
-
-
+//        creating map with the help of collectors
+        LinkedHashMap<Integer, String> map = empList.stream().collect(toMap(Employee::getEmployee_id,Employee::getEmployee_surname,(x, y) -> x + ","+ y, LinkedHashMap::new));
+        map.forEach((x,y)->System.out.println(x +"=" + y));
 
 
 
