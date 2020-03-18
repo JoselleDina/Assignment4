3669c09d84149cec5bbefee198e2546489227c0f
Joselle Dinana <joselledina@gmail.com>
2020-03-18 20:28:05 +0200

    committed

--- src/main/java/ac/za/cput/adp3/App.java
+++ src/main/java/ac/za/cput/adp3/App.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.LinkedHashMap;
 
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 
 /**
@@ -16,19 +17,34 @@ public class App
     public static void main( String[] args )
 
     {
+//       creating list of object Employee
+
         List<Employee> empList = new ArrayList<>();
         empList.add(new Employee(01, "joselle", "dinana"));
         empList.add(new Employee(02, "josy", "dina"));
         empList.add(new Employee(03, "nelcia", "niangui"));
-
+        System.out.println("======================My list=====================");
         for(int i = 0; empList.size() > i; i++)
         {
             System.out.println(empList.get(i));
         }
 
+//        creating Set
+
+        Set<Employee> empSet = new HashSet<>();
+        empSet.addAll(empList);
+
+        System.out.println("======================My Set=====================");
+
+
+        System.out.println(empSet);
+
 //        creating map with the help of collectors
+        System.out.println("======================My Map=====================");
+
         LinkedHashMap<Integer, String> map = empList.stream().collect(toMap(Employee::getEmployee_id,Employee::getEmployee_surname,(x, y) -> x + ","+ y, LinkedHashMap::new));
         map.forEach((x,y)->System.out.println(x +"=" + y));
+
 
 
 
